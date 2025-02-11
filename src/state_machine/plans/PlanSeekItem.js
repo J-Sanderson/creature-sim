1c943d7cac76aa579081c19f2e13f3e5d1d5c41c
J Sanderson <julia@discoverydesign.co.uk>
2025-02-11 11:21:23 +0000

    refactor: move emotion calculation to seek item plan

--- src/state_machine/plans/PlanSeekItem.js
+++ src/state_machine/plans/PlanSeekItem.js
@@ -5,6 +5,7 @@ import {
   planList,
   stateList,
   emotionList,
+  personalityValueList,
 } from '../../defaults';
 
 export default class PlanSeekItem extends Plan {
@@ -81,8 +82,26 @@ export default class PlanSeekItem extends Plan {
           const newX = position.x + dx;
           const newY = position.y + dy;
 
+          const personalityValues = self.getPersonalityValues();
+          const maxMotive = self.getMaxMotive();
+          if (
+            typeof personalityValues[personalityValueList.patience] !== 'number' ||
+            typeof personalityValues[personalityValueList.kindness] !== 'number'
+          ) {
+            console.error(
+              `Error: no personality value found for ${personalityValues[personalityValueList.patience]} or ${personalityValues[personalityValueList.kindness]}`
+            );
+            return;
+          }
+      
+          const roll = Math.random();
+          const amISad =
+            personalityValues[personalityValueList.patience] / maxMotive > roll &&
+            personalityValues[personalityValueList.kindness] / maxMotive > roll;
+          const emotion = amISad ? emotionList.sad : emotionList.angry;
+
           self.setState(stateList.seekItem);
-          self.getState().execute(self, motive, { x: newX, y: newY });
+          self.getState().execute(self, motive, emotion, { x: newX, y: newY });
         } else {
           console.error('No valid movement direction available');
         }
--- src/state_machine/states/StateSeekItem.js
+++ src/state_machine/states/StateSeekItem.js
@@ -1,6 +1,6 @@
 import State from './State';
 import worldManager from '../../managers/WorldManager';
-import { stateList, emotionList, personalityValueList } from '../../defaults';
+import { stateList, emotionList } from '../../defaults';
 
 export default class StateSeekItem extends State {
   constructor(params) {
@@ -10,30 +10,13 @@ export default class StateSeekItem extends State {
     this.suppressEmotionDecay.push(emotionList.sad);
   }
 
-  execute(self, motive, pos) {
+  execute(self, motive, emotion, pos) {
     const emotions = self.getEmotions();
     self.emotionManager.setEmotion(
       self,
       emotionList.happy,
       emotions[emotionList.happy] - 1
     );
-
-    const personalityValues = self.getPersonalityValues();
-    const maxMotive = self.getMaxMotive();
-    if (
-      typeof personalityValues[personalityValueList.patience] !== 'number' ||
-      typeof personalityValues[personalityValueList.kindness] !== 'number'
-    ) {
-      console.error(
-        `Error: no personality value found for ${personalityValues[personalityValueList.patience]} or ${personalityValues[personalityValueList.kindness]}`
-      );
-      return;
-    }
-    const roll = Math.random();
-    const amISad =
-      personalityValues[personalityValueList.patience] / maxMotive > roll &&
-      personalityValues[personalityValueList.kindness] / maxMotive > roll;
-    const emotion = amISad ? emotionList.sad : emotionList.angry;
     self.emotionManager.setEmotion(self, emotion, emotions[emotion] + 2);
 
     self.showMotive(motive ? motive : '');

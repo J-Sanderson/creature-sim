bca35403be280b7b76ff95234f709110435ac787
J Sanderson <julia@discoverydesign.co.uk>
2025-09-29 12:48:03 +0100

    feat: install jest, add basic test

---
+++ tests/utils/Utilities.test.js
@@ -0,0 +1,23 @@
+import { utilities } from '../../src/utils/Utilities';
+
+test('generateGUID produces dash-separated string', () => {
+  const guid = utilities.generateGUID();
+  expect(guid).toMatch(/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/i);
+});
+
+describe('rand', () => {
+  const max = 10;
+  const result = utilities.rand(max);
+
+  test('rand is an integer', () => {
+    expect(Number.isInteger(result)).toBe(true);
+  });
+
+  test ('rand is greater than or equal to 0', () => {
+    expect(result).toBeGreaterThanOrEqual(0);
+  });
+
+  test('rand is less than or equal to max', () => {
+    expect(result).toBeLessThanOrEqual(max);
+  });
+});

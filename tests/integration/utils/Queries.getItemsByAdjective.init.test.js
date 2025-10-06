6fe498e31e836c4b6a4ee0d2e8a4216e3187a951
J Sanderson <julia@discoverydesign.co.uk>
2025-10-06 14:22:26 +0100

    refactor: use addEntity in tests

--- tests/integration/utils/Queries.getItemsByAdjective.init.test.js
+++ tests/integration/utils/Queries.getItemsByAdjective.init.test.js
@@ -3,7 +3,7 @@
  */
 import 'jest-canvas-mock';
 
-import { World } from "../../../src/world/World";
+import { World } from '../../../src/world/World';
 import TeddyBear from '../../../src/entities/items/TeddyBear';
 import Bone from '../../../src/entities/items/Bone';
 import { adjectiveList } from '../../../src/defaults';
@@ -11,22 +11,17 @@ import { queries } from '../../../src/utils/Queries';
 import { creatureBuilder } from '../../helpers/creatureBuilder';
 
 describe('getItemsByAdjective', () => {
-    test('integrates WM→World→(item of requested adjective)', () => {
-        const adjective = adjectiveList.soft;
-        const world = new World(document.createElement('div'));
-        const button1 = document.createElement('button');
-        button1.dataset.adjectives = TeddyBear.adjectives.join();
-        const button2 = document.createElement('button');
-        button2.dataset.adjectives = Bone.adjectives.join();
+  test('integrates WM→World→(item of requested adjective)', () => {
+    const world = new World(document.createElement('div'));
+    world.addEntity(TeddyBear, {xPos: 5, yPos: 5});
+    world.addEntity(Bone, {xPos: 6, yPos: 6});
 
-        world.toggleItem(button1, TeddyBear);
-        world.toggleItem(button2, Bone);
+    const self = creatureBuilder({ world: world.getGUID() });
+    const adjective = adjectiveList.soft;
 
-        const self = creatureBuilder({ world: world.getGUID() });
-
-        const items = queries.getItemsByAdjective(self, adjective);
-        expect(items).toHaveLength(1);
-        expect(items[0]).toBeInstanceOf(TeddyBear);
-        expect(items[0]).not.toBeInstanceOf(Bone);
-    });
+    const items = queries.getItemsByAdjective(self, adjective);
+    expect(items).toHaveLength(1);
+    expect(items[0]).toBeInstanceOf(TeddyBear);
+    expect(items[0]).not.toBeInstanceOf(Bone);
+  });
 });
--- tests/integration/utils/Queries.getItemsByFlavor.int.test.js
+++ tests/integration/utils/Queries.getItemsByFlavor.int.test.js
@@ -3,29 +3,25 @@
  */
 import 'jest-canvas-mock';
 
-import { World } from "../../../src/world/World";
+import { World } from '../../../src/world/World';
 import Chicken from '../../../src/entities/items/Chicken';
 import Steak from '../../../src/entities/items/Steak';
+import { flavorList } from '../../../src/defaults';
 import { queries } from '../../../src/utils/Queries';
 import { creatureBuilder } from '../../helpers/creatureBuilder';
 
 describe('getItemsByFlavor', () => {
-    test('integrates WM→World→(item of requested flavor)', () => {
-        const flavor = Chicken.flavors.join();
-        const world = new World(document.createElement('div'));
-        const button1 = document.createElement('button');
-        button1.dataset.flavors = flavor;
-        const button2 = document.createElement('button');
-        button2.dataset.flavors = Steak.flavors.join();
+  test('integrates WM→World→(item of requested flavor)', () => {
+    const world = new World(document.createElement('div'));
+    world.addEntity(Chicken, {xPos: 5, yPos: 5});
+    world.addEntity(Steak, {xPos: 6, yPos: 6});
 
-        world.toggleItem(button1, Chicken);
-        world.toggleItem(button2, Steak);
+    const self = creatureBuilder({ world: world.getGUID() });
+    const flavor = flavorList.chicken;
 
-        const self = creatureBuilder({ world: world.getGUID() });
-
-        const items = queries.getItemsByFlavor(self, flavor);
-        expect(items).toHaveLength(1);
-        expect(items[0]).toBeInstanceOf(Chicken);
-        expect(items[0]).not.toBeInstanceOf(Steak);
-    });
+    const items = queries.getItemsByFlavor(self, flavor);
+    expect(items).toHaveLength(1);
+    expect(items[0]).toBeInstanceOf(Chicken);
+    expect(items[0]).not.toBeInstanceOf(Steak);
+  });
 });

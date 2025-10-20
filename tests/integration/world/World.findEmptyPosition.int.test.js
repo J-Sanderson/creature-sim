1942d9057bfd3d101dd8e38876e4bdfd7dfbc356
J Sanderson <julia@discoverydesign.co.uk>
2025-10-20 14:53:18 +0100

    feat: integration test World.findEmptyPosition

---
+++ tests/integration/world/World.findEmptyPosition.int.test.js
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+import 'jest-canvas-mock';
+import { World } from '../../../src/world/World';
+import { utilities } from '../../../src/utils/Utilities';
+import Water from '../../../src/entities/items/Water';
+
+describe('findEmptyPosition', () => {
+  test('returns null if no free spaces', () => {
+    const el = document.createElement('div');
+    const world = new World(el, { width: 1, height: 2 });
+    world.addEntity(Water, { xPos: 0, yPos: 0 });
+    world.addEntity(Water, { xPos: 0, yPos: 1 });
+
+    const attempts = 10;
+    const pos = world.findEmptyPosition(attempts);
+    expect(pos).toBeNull();
+  });
+
+  test('returns null if no free spaces found after max attempts', () => {
+    const el = document.createElement('div');
+    const world = new World(el, { width: 1, height: 2 });
+    world.addEntity(Water, { xPos: 0, yPos: 0 });
+    jest.spyOn(utilities, 'rand').mockReturnValue(0);
+
+    const attempts = 10;
+    const pos = world.findEmptyPosition(attempts);
+    expect(pos).toBeNull();
+  });
+
+  test('returns position object if space found', () => {
+    const el = document.createElement('div');
+    const world = new World(el, { width: 1, height: 2 });
+    world.addEntity(Water, { xPos: 0, yPos: 0 });
+    jest
+      .spyOn(utilities, 'rand')
+      .mockReturnValueOnce(0)
+      .mockReturnValueOnce(0)
+      .mockReturnValueOnce(0)
+      .mockReturnValueOnce(1);
+
+    const pos = world.findEmptyPosition();
+    expect(pos).toEqual({ xPos: 0, yPos: 1 });
+  });
+});

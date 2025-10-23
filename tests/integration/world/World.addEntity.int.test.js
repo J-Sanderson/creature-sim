22233a4878851d318ffbfd9d575087cd4ef7d845
J Sanderson <julia@discoverydesign.co.uk>
2025-10-23 11:16:12 +0100

    feat: integration test World.addEntity

---
+++ tests/integration/world/World.addEntity.int.test.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+import 'jest-canvas-mock';
+import { World } from '../../../src/world/World';
+import Water from '../../../src/entities/items/Water';
+
+describe('addEntity', () => {
+    test('adds entity of the provided type', () => {
+        const el = document.createElement('div');
+        const world = new World(el);
+
+        world.entities.items.clear();
+        world.addEntity(Water, {xPos: 0, yPos: 0});
+        
+        const items = world.getItems();
+        expect(items.values().next().value).toBeInstanceOf(Water);
+    });
+});

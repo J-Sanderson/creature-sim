2c3f303b3a61cc47fa8516e0eb705887d1142773
J Sanderson <julia@discoverydesign.co.uk>
2025-11-10 13:26:49 +0000

    feat: integration and unit tests for Item

---
+++ tests/integration/entities/items/Item.int.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+import 'jest-canvas-mock';
+
+import { World } from '../../../../src/world/World';
+import Entity from '../../../../src/entities/Entity';
+import { Item } from '../../../../src/entities/items/Item';
+
+describe('init', () => {
+    test('item is an instance of Entity', () => {
+        const el = document.createElement('div');
+        const world = new World(el);
+
+        const item = new Item(world.getGUID());
+        expect(item).toBeInstanceOf(Entity);
+    });
+});
---
+++ tests/unit/entities/items/Item.test.js
@@ -0,0 +1,41 @@
+/**
+ * @jest-environment jsdom
+ */
+jest.mock('../../../../src/world/World', () => {
+  const MockWorld = class World {
+    constructor() {
+      this.params = { maxMotive: 100, lineWidth: 1, cellSize: 10 };
+    }
+    getParam(param) {
+      return this.params[param];
+    }
+    displayEntity() {
+      return jest.fn();
+    }
+    getBounds() {
+      return { x: 5, y: 5 };
+    }
+  };
+  return { __esModule: true, World: MockWorld };
+});
+
+import { World as MockWorld } from '../../../../src/world/World';
+import worldManager from '../../../../src/managers/WorldManager';
+import { Item } from '../../../../src/entities/items/Item';
+import { adjectiveList } from '../../../../src/defaults';
+
+describe('constructor', () => {
+    test('contains inanimate adjective', () => {
+        jest.spyOn(worldManager, 'getWorld').mockReturnValue(new MockWorld());
+        const item = new Item('w-1');
+
+        expect(item.getAdjectives()).toContain(adjectiveList.inanimate);
+    });
+
+    test('does not contain animate adjective', () => {
+        jest.spyOn(worldManager, 'getWorld').mockReturnValue(new MockWorld());
+        const item = new Item('w-1');
+        
+        expect(item.getAdjectives()).not.toContain(adjectiveList.animate);
+    });
+});

5a9ff5be2bc3533c52040ef9d650220377a02e2a
J Sanderson <julia@discoverydesign.co.uk>
2025-11-10 12:46:24 +0000

    feat: integration test Entity

---
+++ tests/integration/entities/Entity.constructor.int.test.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+import 'jest-canvas-mock';
+import { World } from '../../../src/world/World';
+import Entity from '../../../src/entities/Entity';
+
+describe('constructor', () => {
+    test('entity bounds match world bounds', () => {
+        const el = document.createElement('div');
+        const world = new World(el);
+
+        const entity = new Entity(world.getGUID());
+        expect(entity.bounds).toEqual(world.getBounds());
+    });
+});
---
+++ tests/integration/entities/Entity.init.int.test.js
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+import 'jest-canvas-mock';
+import { World } from '../../../src/world/World';
+import Entity from '../../../src/entities/Entity';
+
+describe('init', () => {
+  test('icon is HTML div element', () => {
+    const el = document.createElement('div');
+    const world = new World(el);
+
+    const entity = new Entity(world.getGUID());
+    expect(entity.outputs.icon).toBeInstanceOf(HTMLDivElement);
+  });
+
+  test('calls world.displayEntity', () => {
+    const displayEntity = jest.spyOn(World.prototype, 'displayEntity').mockImplementation(() => {});
+    const el = document.createElement('div');
+    const world = new World(el);
+
+    const entity = new Entity(world.getGUID());
+    expect(displayEntity).toHaveBeenCalledTimes(2);
+    expect(displayEntity).toHaveBeenLastCalledWith(entity.outputs.icon);
+  });
+
+  test('creates unique entities', () => {
+    const el = document.createElement('div');
+    const world = new World(el);
+    const worldID = world.getGUID();
+    const entity1 = new Entity(worldID);
+    const entity2 = new Entity(worldID);
+
+    expect(entity1.getGUID()).not.toEqual(entity2.getGUID());
+  });
+});

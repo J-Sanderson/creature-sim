6e194cb76a3a40a363666617c1d3b788e32fedd0
J Sanderson <julia@discoverydesign.co.uk>
2025-11-26 07:52:35 +0000

    refactor: showStatusWrapper tests

--- tests/unit/managers/DebugManager.showStatusWrapper.test.js
+++ tests/unit/managers/DebugManager.showStatusWrapper.test.js
@@ -6,47 +6,40 @@ import { DebugManager } from '../../../src/managers/DebugManager';
 import { worldBuilder } from '../../helpers/worldBuilder';
 
 describe('showStatusWrapper', () => {
-  test('adds status wrapper to world elements', () => {
-    const debugManager = new DebugManager();
-    let world = worldBuilder({ entities: { items: [], creatures: [] } });
+  let debugManager;
+  let world;
+  beforeEach(() => {
+    debugManager = new DebugManager();
+    world = worldBuilder({ entities: { items: [], creatures: [] } });
     debugManager.showStatusWrapper(world);
+  });
 
+  test('adds status wrapper to world elements', () => {
     expect(world.elements).toHaveProperty('statusWrapper');
   });
 
   test('status wrapper is HTML div element', () => {
-    const debugManager = new DebugManager();
-    let world = worldBuilder({ entities: { items: [], creatures: [] } });
-    debugManager.showStatusWrapper(world);
-
     expect(world.elements.statusWrapper).toBeInstanceOf(HTMLDivElement);
   });
 
   test('status wrapper has status-wrapper class', () => {
-    const debugManager = new DebugManager();
-    let world = worldBuilder({ entities: { items: [], creatures: [] } });
-    debugManager.showStatusWrapper(world);
-
-    expect(Array.from(world.elements.statusWrapper.classList)).toContain(
-      'status-wrapper'
-    );
+    expect(
+      world.elements.statusWrapper.classList.contains('status-wrapper')
+    ).toBe(true);
   });
 
   test('status wrapper has initial status content', () => {
-    const debugManager = new DebugManager();
-    let world = worldBuilder({ entities: { items: [], creatures: [] } });
-    debugManager.showStatusWrapper(world);
-
-    expect(world.elements.statusWrapper.innerHTML).toBe('<p>Status</p>');
+    expect(world.elements.statusWrapper.querySelector('p')?.textContent).toBe(
+      'Status'
+    );
   });
 
   test('status wrapper is child of world root element', () => {
-    const debugManager = new DebugManager();
-    let world = worldBuilder({ entities: { items: [], creatures: [] } });
-    debugManager.showStatusWrapper(world);
-
     expect(world.elements.statusWrapper.parentElement).toBe(
       world.elements.root
+    );
+    expect(world.elements.root.contains(world.elements.statusWrapper)).toBe(
+      true
     );
   });
 });

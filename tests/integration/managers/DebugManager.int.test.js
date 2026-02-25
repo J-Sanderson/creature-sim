c4761fdff051b7041d45cae206510041e7acc905
J Sanderson <julia@discoverydesign.co.uk>
2026-02-25 14:01:22 +0000

    feat: int test DebugManager goal updates

--- tests/integration/managers/DebugManager.int.test.js
+++ tests/integration/managers/DebugManager.int.test.js
@@ -6,12 +6,13 @@ import { World } from '../../../src/world/World';
 
 describe('debugManager', () => {
   beforeEach(() => {
-    jest.spyOn(World.prototype, 'tick').mockImplementation(() => {});
     jest.clearAllMocks();
   });
   afterEach(() => jest.restoreAllMocks());
 
   test('displays accurate creature outputs on init', () => {
+    jest.spyOn(World.prototype, 'tick').mockImplementation(() => {});
+
     const el = document.createElement('div');
     const world = new World(el, { showStatus: true, showSliders: true, showPersonality: true });
 
@@ -68,6 +69,49 @@ describe('debugManager', () => {
         const output = favorites[0].querySelector(`.personality-item-${value}`);
         expect(output.innerHTML).toBe(`${value}: ${favoriteValues[value]}`);
       }
+
+      i++;
+    });
+  });
+
+  test('displays accurate goal information on goal setting', () => {
+    jest.spyOn(global, 'setInterval').mockImplementation(() => {});
+
+    const el = document.createElement('div');
+    const world = new World(el, { showStatus: true });
+
+    const creatures = world.getCreatures();
+    const statusWrapper = world.getElement('statusWrapper');
+    const statuses = statusWrapper.querySelectorAll('.status');
+    
+    let i = 0;
+    creatures.forEach((creature) => {
+      // TODO - this appears to fail if the goal is GoalKnockItemFromToybox (toybox does not exist?)
+      world.tick();
+      const goals = creature.goalManager.getGoals();
+      const status = statuses[i];
+
+      const table = status.querySelector('.status-item-goals table');
+      const rows = table.querySelectorAll('tbody tr');
+      expect(Object.keys(goals).length).toBe(rows.length);
+
+      rows.forEach(row => {
+        const entries = Array.from(row.querySelectorAll('td')).map(td => td.textContent);
+        const name = entries[0];
+        const goal = goals[name];
+        expect(entries).toEqual([
+          name,
+          JSON.stringify(goal.goalToken.priority),
+          JSON.stringify(goal.goalToken.suspended),
+          JSON.stringify(goal.goalToken.ticks),
+          goal.goalToken.calledBy ? goal.goalToken.calledBy : '',
+          goal.worldToken.target ? goal.worldToken.target : '',
+        ]);
+      });
+
+      const currentGoal = creature.goalManager.getCurrentGoalName();
+      const output = status.querySelector('.status-item-currentGoalName output');
+      expect(output.innerHTML).toEqual(currentGoal);
 
       i++;
     });
